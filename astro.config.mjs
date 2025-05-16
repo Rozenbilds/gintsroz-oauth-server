e8c98d62bc99d867d0e59df9d10c2711331ef87f
Rozenbilds <gints.roz@inbox.lv>
2025-05-16 19:51:01 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
 import decapCmsOauth from "astro-decap-cms-oauth";
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   integrations: [decapCmsOauth()],
-  output: "server"
+  output: "server",
+  adapter: vercel(),
 });
